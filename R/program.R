64bdcec8942edddad8a14c6fcb4e7f4d7f047617
Martin Morgan <Martin.Morgan@RoswellPark.org>
2023-03-18 11:24:57 -0400

    add program_projects()

--- R/program.R
+++ R/program.R
@@ -29,11 +29,11 @@
 #'     "RFA-CA-19-039", "Early-Stage Development of ...",
 #'     "RFA-CA-19-038", "Development of Innovative ...",
 #'     "RFA-CA-19-040", "Advanced Development of ...",
-#'     "RFA-CA-19-041", "Sustained Support..."
+#'     "RFA-CA-19-041", "Sustained Support of..."
 #' )
 #'
 #' program_summary(foas)
-#' 
+#'
 #' @importFrom dplyr full_join .data
 #'
 #' @export
@@ -51,7 +51,7 @@ program_summary <-
         "full_foa", "core_project_num", "fiscal_year", "award_amount"
     )
     projects <- reporter_projects(
-        foa = foas$full_foa,
+        foa = tbl$full_foa,
         include_fields = project_include_fields
     )
 
@@ -94,4 +94,86 @@ program_summary <-
         full_join(program_citations, by = "fiscal_year") |>
         mutate(fiscal_year = as.integer(.data$fiscal_year)) |>
         arrange(fiscal_year)
+}
+
+## program_projects
+
+program_projects_by_foa <-
+    function(projects)
+{
+    projects |>
+        arrange(.data$fiscal_year, .data$award_amount) |>
+        group_by(.data$full_foa, .data$core_project_num) |>
+        summarize(
+            years = length(unique(.data$fiscal_year)),
+            award_amount = sum(.data$award_amount),
+            contact_pi_name = tail(.data$contact_pi_name, 1L),
+            project_title = tail(.data$project_title, 1L)
+        )
+}
+
+program_projects_by_project_num <-
+    function(projects)
+{
+    projects |>
+        arrange(.data$fiscal_year, .data$award_amount) |>
+        group_by(.data$core_project_num) |>
+        summarize(
+            full_foa = tail(.data$full_foa, 1L),
+            years = length(unique(.data$fiscal_year)),
+            award_amount = sum(.data$award_amount),
+            contact_pi_name = tail(.data$contact_pi_name, 1L),
+            project_title = tail(.data$project_title, 1L)
+        ) |>
+        select("full_foa", everything())
+}
+
+#' @rdname program
+#'
+#' @description `program_projects()` returns a tibble of project
+#'     numbers and standardized contact PI name and project title.
+#'
+#' @param by character(1) indicating how program projects are
+#'     summarized -- by `full_foa` so that projects funded by more
+#'     than one FOA are reported for each FOA, or `core_project_num`
+#'     so that the summary is by project number..
+#'
+#' @details `program_projects()` provides a single row for each
+#'     project. It chooses as `full_foa` the most recent FOA under
+#'     which the project was funded. It chooses as `contact_pi_name`
+#'     and `project_title` the name of the contact PI and project
+#'     title of the largest award in the most recent year of funding.
+#'
+#' @examples
+#' program_projects(foas)
+#'
+#' @export
+program_projects <-
+    function(tbl, by = c("full_foa", "core_project_num"))
+{
+    stopifnot(
+        inherits(tbl, "tbl_df"),
+        "full_foa" %in% colnames(tbl)
+    )
+    by <- match.arg(by)
+
+    ## data collection
+
+    project_include_fields = c(
+        "full_foa", "core_project_num",
+        "fiscal_year", "award_amount",
+        "contact_pi_name", "project_title"
+    )
+
+    projects <- reporter_projects(
+        foa = tbl$full_foa,
+        include_fields = project_include_fields
+    )
+
+    ## summary
+    if (identical(by, "full_foa")) {
+        program_projects_by_foa(projects)
+    } else {
+        program_projects_by_project_num(projects)
+    }
 }
