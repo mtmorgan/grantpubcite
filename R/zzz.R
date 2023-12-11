bb93a448e95908909e8ef2caa4a46a04ee3a05b7
Martin Morgan <Martin.Morgan@RoswellPark.org>
2023-12-11 16:05:19 -0500

    add user_agent() with package information to POST and GET

--- R/zzz.R
+++ R/zzz.R
@@ -1,8 +1,11 @@
+PACKAGE_NAME <- NULL
+
 #' @importFrom memoise memoise
 
 .onLoad <-
-    function(...)
+    function(libname, pkgname)
 {
+    PACKAGE_NAME <<- pkgname
     if (as.logical(Sys.getenv("GRANTPUBCITE_MEMOISE", TRUE))) {
         reporter_endpoint <<- memoise(reporter_endpoint)
         icite_one_chunk <<- memoise(icite_one_chunk)
